65600aeafd2d9cf7b805988619268179bf326da0
Marshall Burns <marshallbcodes@gmail.com>
2023-04-30 17:24:14 -0500

    Build Express server/install server dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import About from "./pages/about"
+import Header from './components/header'
+import Footer from './components/footer'
+import Home from './pages/home'
 
 function App() {
 
 
   return (
     <>
-high
+      <Header/>
+      <Home/>
+      <About />
+      <Footer/>
     </>
   )
 }
