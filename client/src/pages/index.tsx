3d3a5e0439e9f81aaae05867ac60d317fe6075c3
jasmine zanders <jasminezanders211@gmail.com>
2023-05-16 14:38:13 -0500

    a nother new update

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ralph from "../../assets/ralph.jpg"
 import spiderman from "../../assets/spiderman.jpg"
 import princessfrog from "../../assets/princessfrog.jpg"
+import treasureplanet from "../../assets/treasureplanet.jpg"
+import avatar2009 from "../../assets/avatar2009.jpg"
+import traintobusan from "../../assets/traintobusan.jpg"
+import aquietplace from "../../assets/aquietplace.jpg"
+
 export default function Home() {
   return (
     <div>home 
@@ -15,11 +20,14 @@ export default function Home() {
     <img src={princessfrog}/>
     <img src={spiderman }/>
     <img src={ralph }/>
+    <img src={treasureplanet }/>
     </div>
 
 
     <div id='bottomRow'className='movieRows'>
-
+    <img src={traintobusan}/>
+    <img src={aquietplace}/>
+    <img src={avatar2009}/>
     </div>
 </div>
 
